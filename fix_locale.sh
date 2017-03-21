2fb2b16024d80a7535ecdbcb917259de124a8fdc
fabrice <fabrice@linuxbe.com>
2017-03-21 23:51:19 +0100

    cleanup

--- fix_locale.sh
+++ fix_locale.sh
@@ -1,5 +1,22 @@
-export LANGUAGE=en_US.UTF-8
-export LANG=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
-locale-gen en_US.UTF-8
-dpkg-reconfigure locales
+#!/bin/bash
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+echo '::fixing locales ... '
+
+export LANGUAGE="en_US.UTF-8"
+sleep 1
+export LANG="en_US.UTF-8"
+sleep 1
+export LC_ALL="en_US.UTF-8"
+sleep 1
+
+echo '::almost there ... '
+locale-gen en_US.UTF-8 && dpkg-reconfigure locales
+
+locale
+
+echo ':: done !'
+echo 'peace, love and Linux !'
