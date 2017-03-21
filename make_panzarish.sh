cab263039f741508c616d4441540d1126952c18c
fabrice <fabrice@linuxbe.com>
2017-03-21 23:33:41 +0100

    shebang

--- make_panzarish.sh
+++ make_panzarish.sh
@@ -1,7 +1,9 @@
-sudo apt-get install zsh
+#!/bin/bash
+
+cd ~
+sudo apt-get install -y zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sudo apt-get remove vim-tiny && sudo apt-get install vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cd ~
 git clone https://github.com/digifaps/rc_files.git
 sudo apt-get install exuberant-ctags
